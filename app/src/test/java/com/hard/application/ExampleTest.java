b5693cb327d0fc920fb64343b160ea76f8c7cdd4
hard <harleydav@mail.ru>
2021-01-08 12:39:50 +0300

    updated

---
+++ app/src/test/java/com/hard/application/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.hard.application;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+    @Test
+    public void shouldShowAssertion() {
+        assertEquals(4, 2 + 2);
+    }
+}
