da5dc2f15515e4a71be942a3ff7043f6ebd74b57
hard <harleydav@mail.ru>
2021-01-08 14:02:05 +0300

    JUnit: updated version

--- app/src/test/java/com/hard/application/ExampleTest.java
+++ app/src/test/java/com/hard/application/ExampleTest.java
@@ -1,8 +1,8 @@
 package com.hard.application;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest {
     @Test
